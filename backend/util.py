a9a76948a191bcfdc81a45e6df5fd975f35aea8a
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2013-10-26 19:56:29 +0300

    Add configuration reading support.
    Configuration is stored in backend.ini file. Access to it is provided
    through backend.util.Config class.

---
+++ backend/util.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+
+from __future__ import absolute_import, unicode_literals
+import os
+import ConfigParser
+
+
+class Config(object):
+    CONFIG_FILE_NAME = 'backend.ini'
+    CONFIG_SECTION_NAME = 'general'
+
+    VALUE_DB_URL = 'db-url'
+
+    @classmethod
+    def get_config_file_path(cls):
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        dir = os.path.abspath(os.path.join(script_dir, '..'))
+        return os.path.join(dir, cls.CONFIG_FILE_NAME)
+
+    @classmethod
+    def get_config_parser(cls):
+        config = ConfigParser.SafeConfigParser()
+        config.read(cls.get_config_file_path())
+        return config
+
+    @classmethod
+    def get_config_value(cls, value_name):
+        return cls.get_config_parser().get(cls.CONFIG_SECTION_NAME, value_name)
