6acda668e429ed8766b1ab82b3ec5a2f2709a2ba
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2013-12-28 17:40:44 +0300

    Introduce “deploy-bus-time-db.ini” configuration file.
    Move some hard-coded settings to configuration file.
    
    Command-line argument “--signature-key-file” is dropped in favour
    of storing this option in configuration file.

--- deploy-bus-time-db.py
+++ deploy-bus-time-db.py
@@ -3,7 +3,7 @@
 
 
 from __future__ import absolute_import, unicode_literals, print_function
-from argparse import ArgumentParser
+import ConfigParser
 from getpass import getpass
 import os
 import subprocess
@@ -19,16 +19,10 @@ import requests
 
 ARCHIVE_FILE_NAME = 'archive.zip'
 ARCHIVE_FORMAT = 'zipball'
-DB_FILE_NAME = 'bustime.db.gz'
 DOWNLOAD_DIR_PREFIX = 'bustime-backend-repo'
 EXTRACTED_ARCHIVE_DIR = 'archive'
-MAKE_TARGET = 'release'
-MASTER_HEAD_REF = 'heads/master'
-REPO_NAME = 'bus-time-database'
-REPO_OWNER = 'win2l'
-VERSION_TAG_NAME_PREFIX = 'db-schema-version-'
+BRANCH_HEAD_REF_FORMAT = 'heads/{}'
 
-DEPLOY_URL = 'http://bustime-backend-dsav.herokuapp.com/db-updates/deploy'
 FORM_STRING_DATA_ENCODING = 'utf8'
 FORM_CONTENTS_KEY = 'contents'
 FORM_VERSION_KEY = 'version'
@@ -36,14 +30,44 @@ FORM_SCHEMA_VERSION_KEY = 'schema-version'
 FORM_SIGNATURE_SUFFIX = '-signature'
 
 
+class Config(object):
+    CONFIG_FILE_NAME = 'deploy-bus-time-db.ini'
+    CONFIG_SECTION_NAME = 'general'
+
+    VALUE_REPO_OWNER = 'repo-owner'
+    VALUE_REPO_NAME = 'repo-name'
+    VALUE_REPO_BRANCH = 'repo-branch'
+    VALUE_VERSION_TAG_NAME_PREFIX = 'version-tag-name-prefix'
+    VALUE_DB_MAKE_TARGET = 'db-make-target'
+    VALUE_DB_MADE_FILE_NAME = 'db-made-file-name'
+    VALUE_DEPLOY_URL = 'deploy-url'
+    VALUE_SIGNATURE_KEY_FILE = 'signature-key-file'
+
+    @classmethod
+    def get_config_value(cls, value_name):
+        return cls.get_config_parser().get(cls.CONFIG_SECTION_NAME, value_name)
+
+    @classmethod
+    def get_config_parser(cls):
+        config = ConfigParser.SafeConfigParser()
+        config.read(cls.get_config_file_path())
+        if not config.has_section(cls.CONFIG_SECTION_NAME):
+            config.add_section(cls.CONFIG_SECTION_NAME)
+        return config
+
+    @classmethod
+    def get_config_file_path(cls):
+        dir = os.path.abspath(cls.get_script_dir())
+        return os.path.join(dir, cls.CONFIG_FILE_NAME)
+
+    @classmethod
+    def get_script_dir(cls):
+        return os.path.dirname(os.path.realpath(__file__))
+
+
 def main():
-    deploy_version(parse_args().signature_key_file)
-
-
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument('--signature-key-file', '-f', required=True)
-    return parser.parse_args()
+    key_file = Config.get_config_value(Config.VALUE_SIGNATURE_KEY_FILE)
+    deploy_version(key_file)
 
 
 def deploy_version(key_file):
@@ -88,7 +112,9 @@ def get_credentials():
 
 @describe(start='Logging in to repo...', done='done.')
 def login_to_repo(username, password):
-    return github3.login(username, password).repository(REPO_OWNER, REPO_NAME)
+    repo_owner = Config.get_config_value(Config.VALUE_REPO_OWNER)
+    repo_name = Config.get_config_value(Config.VALUE_REPO_NAME)
+    return github3.login(username, password).repository(repo_owner, repo_name)
 
 
 def get_latest_schema_commit_info(repo):
@@ -123,11 +149,13 @@ def get_latest_schema_tag_info(repo):
 
 
 def extract_schema_version(tag):
-    return int(tag.name[len(VERSION_TAG_NAME_PREFIX):])
+    name_prefix = Config.get_config_value(Config.VALUE_VERSION_TAG_NAME_PREFIX)
+    return int(tag.name[len(name_prefix):])
 
 
 def is_schema_version_tag(tag):
-    if not tag.name.startswith(VERSION_TAG_NAME_PREFIX):
+    name_prefix = Config.get_config_value(Config.VALUE_VERSION_TAG_NAME_PREFIX)
+    if not tag.name.startswith(name_prefix):
         return False
 
     try:
@@ -149,11 +177,16 @@ def describe_latest_schema_tag_info(schema_version, schema_version_commit_sha):
 
 @describe(start='Obtaining master HEAD sha...', done='done.')
 def get_master_head_sha(repo):
-    head = repo.ref(MASTER_HEAD_REF)
+    head = repo.ref(get_head_ref())
     if head:
         return head.object.sha
     else:
         return None
+
+
+def get_head_ref():
+    branch_name = Config.get_config_value(Config.VALUE_REPO_BRANCH)
+    return BRANCH_HEAD_REF_FORMAT.format(branch_name)
 
 
 def describe_master_head_sha(master_head_sha):
@@ -173,8 +206,8 @@ def describe_schema_version_tag_not_master_head():
 def deploy_database(schema_version, master_head_sha, contents, key_file):
     files = build_files_dict(schema_version, master_head_sha, contents,
                              key_file)
-
-    response = requests.post(DEPLOY_URL, files=files)
+    deploy_url = Config.get_config_value(Config.VALUE_DEPLOY_URL)
+    response = requests.post(deploy_url, files=files)
     if not response.ok:
         print_response_error(response)
 
@@ -256,8 +289,11 @@ def get_extracted_dir(download_dir):
 
 def make_database_file(download_dir):
     make_dir = get_make_dir(download_dir)
-    subprocess.check_call(['make', MAKE_TARGET, '--directory', make_dir])
-    return os.path.join(make_dir, DB_FILE_NAME)
+    make_target = Config.get_config_value(Config.VALUE_DB_MAKE_TARGET)
+    db_file_name = Config.get_config_value(Config.VALUE_DB_MADE_FILE_NAME)
+
+    subprocess.check_call(['make', make_target, '--directory', make_dir])
+    return os.path.join(make_dir, db_file_name)
 
 
 def get_make_dir(download_dir):
